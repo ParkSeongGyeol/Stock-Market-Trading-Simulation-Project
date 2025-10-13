f5965230e4386ada24b87577d6baf5395824332b
ParkSeongGyeol <skp0626@naver.com>
2025-10-13 23:31:24 +0900

    프로젝트 구조 정리

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("    모의 주식 투자 시스템 v1.0");
+        System.out.println("=================================");
+        
+        // MainController 실행
+        // MainController controller = new MainController();
+        // controller.start();
+    }
+}
---
+++ src/model/User.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+     String userId;
+     String password;
+     String userName;
+    LocalDateTime registeredDate;
+    long balance;
+
+   
+    public User() {
+    }
+
+    
+    public User(String userId, String password, String userName, LocalDateTime registeredDate) {
+        this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.registeredDate = LocalDateTime.now();
+    }
+
+    
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDateTime getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDateTime registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+    public long getBalance() {
+    	return balance;
+    }
+    public void setBalance(long balance) {
+    	this.balance = balance;
+    }
+
+    
+    
+    public String toString() {
+        return String.format("User(userId = '%s',"
+        		+ "userName = '%s',"
+        		+ "registeredDate = %s,"
+        		+ "balance=%,d원}",
+        		userId , userName, registeredDate.toLocalDate(), balance);     		
+    }
+}
