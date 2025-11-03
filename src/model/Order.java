2c657a0a43ddfe6c9f1425e79a76fb661722f386
ParkSeongGyeol <skp0626@naver.com>
2025-11-03 11:57:04 +0900

    order 필드 정의 및 getter/setter 생성

--- src/model/Order.java
+++ src/model/Order.java
@@ -8,59 +8,104 @@ public class Order {
 	private String orderType;
 	private int quantity;
 	private double price;
+  private String status;
+  private String orderDate;
+  private String executionDate;
+  private double executedPrice;
+
+  // 생성자 - 기본 값도 설정
+  public Order(String orderId, String userId, String stockCode, String orderType, int quantity, double price) {
+    this.orderId = orderId;
+    this.userId = userId;
+    this.stockCode = stockCode;
+    this.orderType = orderType;
+    this.quantity = quantity;
+    this.price = price;
+    this.status = "PENDING";
+    this.orderDate = java.time.LocalDateTime.now().toString();
+    this.executionDate = null;
+    this.executedPrice = 0.0;
+  }
 
 	// getter/setter
 	public String getOrderId() {
-		// TODO: 구현 필요
-		return null;
+		return orderId;
 	}
 
 	public void setOrderId(String orderId) {
-		// TODO: 구현 필요
-	}
+    this.orderId = orderId;
+  }
 
 	public String getUserId() {
-		// TODO: 구현 필요
-		return null;
+		return userId;
 	}
 
 	public void setUserId(String userId) {
-		// TODO: 구현 필요
+    this.userId = userId;
 	}
 
 	public String getStockCode() {
-		// TODO: 구현 필요
-		return null;
+    return stockCode;
 	}
 
 	public void setStockCode(String stockCode) {
-		// TODO: 구현 필요
+    this.stockCode = stockCode;
 	}
 
 	public String getOrderType() {
-		// TODO: 구현 필요
-		return null;
+    return orderType;
 	}
 
 	public void setOrderType(String orderType) {
-		// TODO: 구현 필요
+    this.orderType = orderType;
 	}
 
 	public int getQuantity() {
-		// TODO: 구현 필요
-		return 0;
+    return quantity;
 	}
 
 	public void setQuantity(int quantity) {
-		// TODO: 구현 필요
-	}
+    this.quantity = quantity;
+  	}
 
 	public double getPrice() {
-		// TODO: 구현 필요
-		return 0.0;
+    return price;
 	}
 
 	public void setPrice(double price) {
-		// TODO: 구현 필요
+  
+    this.price = price;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getOrderDate() {
+    return orderDate;
+  }
+
+  public void setOrderDate(String orderDate) {
+    this.orderDate = orderDate;
+  }
+
+  public String getExecutionDate() {
+    return executionDate;
+  }
+
+  public void setExecutionDate(String executionDate) {
+    this.executionDate = executionDate;
+  }
+
+  public double getExecutedPrice() {
+    return executedPrice;
+  }
+
+  public void setExecutedPrice(double executedPrice) {
+    this.executedPrice = executedPrice;
 	}
 }
