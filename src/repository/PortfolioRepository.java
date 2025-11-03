547e60773669cd14d17cbb20dba7bc9e102315b1
NSU <NSU@192.192.34.26>
2025-11-03 11:57:57 +0900

    createPortfolio 작성중

--- src/repository/PortfolioRepository.java
+++ src/repository/PortfolioRepository.java
@@ -1,5 +1,14 @@
 package repository;
 
-public class PortfolioRepository {
+import java.util.HashMap;
 
+import model.Portfolio;
+
+public class PortfolioRepository {
+	
+	HashMap<String, Portfolio> PortfolioInfo = new HashMap<String, Portfolio>();
+			
+	public void createPortfolio() {
+		PortfolioInfo.put(null, null);
+	}
 }
